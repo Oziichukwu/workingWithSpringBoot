26731772d3174b3c434f44f5d678e93a8313e137
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-15 05:37:57 +0100

    configuration file

--- src/main/java/com/phoenixStore/service/cloud/CloudinaryServiceImpl.java
+++ src/main/java/com/phoenixStore/service/cloud/CloudinaryServiceImpl.java
@@ -1,2 +1,29 @@
-package com.phoenixStore.service.cloud;public class CloudinaryServiceImpl {
+package com.phoenixStore.service.cloud;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+
+@Service("cloudinary-service")
+public class CloudinaryServiceImpl implements CloudService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    @Override
+    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException {
+
+        return cloudinary.uploader().upload(bytes, params);
+
+    }
+
+//    @Override
+//    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) {
+//        return null;
+//    }
 }
--- src/main/java/com/phoenixStore/service/config/Configuration.java
+++ src/main/java/com/phoenixStore/service/config/Configuration.java
@@ -1,2 +1,29 @@
-package com.phoenixStore.service.config;public class Configuration {
+package com.phoenixStore.service.config;
+
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary(){
+
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true));
+
+    }
 }

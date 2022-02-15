555e7d818febb5b2bf29a3601256b74300b4c93a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-15 05:39:04 +0100

    files

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,4 +1,9 @@
 
+-- Referential integrity Constrsint: It means wen a table reference another table, u can not perform delete on the table
+
+-- turn the foreign key off
+set foreign_key_checks=0;
+
 truncate table product;
 
 insert into product(id, name, price, quantity)
@@ -6,7 +11,9 @@ insert into product(id, name, price, quantity)
 values(12, 'car', 50000, 2),
 (13, 'bar', 55000, 5),
 (14, 'television', 6000, 1),
-(15, 'gigner', 899, 8)
+(15, 'gigner', 899, 8);
+
+set foreign_key_checks=1;
 
 
 

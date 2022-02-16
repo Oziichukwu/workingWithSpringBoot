a59de2d62445bd7f372bce3ec6dae166c8ccf8a0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-02-16 12:29:08 +0100

    cart service

--- src/main/java/com/phoenixStore/service/cart/CartService.java
+++ src/main/java/com/phoenixStore/service/cart/CartService.java
@@ -1,2 +1,9 @@
-package com.phoenixStore.service.cart;public interface CartService {
+package com.phoenixStore.service.cart;
+
+import com.phoenixStore.data.models.Cart;
+
+public interface CartService {
+
+    void saveCart(Cart cart);
+
 }
--- src/main/java/com/phoenixStore/service/cart/CartServiceImpl.java
+++ src/main/java/com/phoenixStore/service/cart/CartServiceImpl.java
@@ -1,2 +1,20 @@
-package com.phoenixStore.service.cart;public class CartServiceImpl {
+package com.phoenixStore.service.cart;
+
+
+import com.phoenixStore.data.models.Cart;
+import com.phoenixStore.data.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl implements CartService{
+
+    @Autowired
+    private CartRepository cartRepository;
+
+
+    @Override
+    public void saveCart(Cart cart) {
+
+    }
 }
